53b8a1a62b52adf018ff3650270e9e864192e223
Rudolf Horvath <rudolf.horvath@sainsburys.co.uk>
2016-05-26 13:35:23 +0100

    First commit - original code

---
+++ Rental.php
@@ -0,0 +1,45 @@
+<?php
+
+
+class Rental
+{
+
+    /**
+     * @var Movie
+     */
+    private $movie;
+
+    /**
+     * @var int
+     */
+    private $daysRented;
+
+    /**
+     * Rental constructor.
+     * @param Movie $movie
+     * @param int $daysRented
+     */
+    public function __construct(Movie $movie, $daysRented)
+    {
+        $this->movie = $movie;
+        $this->daysRented = $daysRented;
+    }
+
+    /**
+     * @return Movie
+     */
+    public function getMovie()
+    {
+        return $this->movie;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDaysRented()
+    {
+        return $this->daysRented;
+    }
+
+
+}
