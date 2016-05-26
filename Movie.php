35ff7e4374a3091c6d636ec5a46bd1cef2fcf970
Rudolf Horvath <rudolf.horvath@sainsburys.co.uk>
2016-05-26 14:10:51 +0100

    Move Method: Rental -> Movie - getCahrge, getFrequentRenterPoints

--- Movie.php
+++ Movie.php
@@ -34,7 +34,7 @@ class Movie
     {
         return $this->title;
     }
-    
+
     /**
      * @return int
      */
@@ -51,5 +51,46 @@ class Movie
         $this->priceCode = $priceCode;
     }
 
+    /**
+     * @param $daysRented
+     * @return float
+     */
+    public function getCharge($daysRented)
+    {
+        $result = 0;
+
+        switch ($this->getPriceCode()) {
+            case Movie::REGULAR:
+                $result += 2;
+                if ($daysRented > 2) {
+                    $result += ($daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie::NEW_RELEASE:
+                $result += $daysRented * 3;
+                break;
+            case Movie::CHILDRENS:
+                $result += 1.5;
+                if ($daysRented > 3) {
+                    $result += ($daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return $result;
+    }
+
+    /**
+     * @param $daysRented
+     * @return int
+     */
+    public function getFrequentRenterPoints($daysRented)
+    {
+        if (($this->getPriceCode() == Movie::NEW_RELEASE) && $daysRented > 1) {
+            return 2;
+        }
+        else {
+            return 1;
+        }
+    }
 
 }
